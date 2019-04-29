d594bcf54a309a59bb681d80ad9a19928171c8b4
Gonzalo Beltrán <gonzalobeltran@gmail.com>
2019-04-29 01:06:29 -0400

    Actualiza scripts win y lin

--- deploy-simuc2.ps1
+++ deploy-simuc2.ps1
@@ -4,8 +4,8 @@ meteor build ../ec2  --architecture os.linux.x86_64
 
 cd ../ec2
 
-scp simuc2.tar.gz test:/home/ubuntu
+scp simuc2.tar.gz simuc2:/home/ubuntu
 
 rm simuc2.tar.gz
 
-ssh test 'sh deploy-simuc'
+ssh simuc2 'sh deploy-simuc'
